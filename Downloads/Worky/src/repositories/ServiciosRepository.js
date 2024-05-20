49c4e7125109d51b02d1a35ecda73f1ff0b05d15
URIEL GREGORIO CWIRENBAUM <47866146@comunidadort.edu.ar>
2024-05-20 12:09:57 -0300

    1.0.2 20 05 2024

--- Downloads/Worky/src/repositories/ServiciosRepository.js
+++ Downloads/Worky/src/repositories/ServiciosRepository.js
@@ -1,3 +1,25 @@
 import pg from "pg";
 import { BDConfig } from "../BD/BD"
+import Servicio from "../entities/Servicio";
 
+
+export default class EventRepository{
+constructor(){
+    const { Client } = pg;
+    this.DBClient = new Client(BDConfig)
+    this.DBClient.connect()
+}
+async getAllServicios(limit, offset) {
+    var query = `SELECT Nombre, Descripcion, Foto, Precio, Nombre.Categorias, Nombre.Usuarios FROM Servicios INNER JOIN Categorias ON Servicios.idCategoria = Categorias.id INNER JOIN Usuarios ON Servicios.idCreador = Usuarios.id limit ${limit} offset ${offset}`;
+    const values = await client.query(query);
+    return values;
+}
+async BorrarServicio(id, id_creator_user){
+    var query = `DELETE FROM Servicios WHERE id = ${id} AND idCreador = ${id_creator_user}`;
+    const values = await client.query(query);
+    return values;
+}
+async EditarServicio(Servicio){
+    var query = 'UPDATE Servicios SET '
+}
+}
