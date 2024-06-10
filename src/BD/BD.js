abaa902c0c8466b0decda9050287d9eed9256c6b
URIEL GREGORIO CWIRENBAUM <47866146@comunidadort.edu.ar>
2024-06-10 09:05:02 -0300

    1.1.1

--- src/BD/BD.js
+++ src/BD/BD.js
@@ -1,13 +1,12 @@
 import 'dotenv/config'
 
 export const config = {
-    user : process.env.BD_USER,
-    password : process.env.BD_PASSWORD,
     server : process.env.BD_SERVER,
     database : process.env.BD_DATABASE,
     options : {
         encrypt: true,
-        trustServerCertificate: true
+        trustServerCertificate: true,
+        trustedConnection: true
     }
 }
 // https://www.youtube.com/watch?v=HMKVnwlhJO0
