6968bba26abf7631e6543ac05e77cbb7d3411a10
URIEL GREGORIO CWIRENBAUM <47866146@comunidadort.edu.ar>
2024-06-24 09:03:04 -0300

    1.3

--- src/repositories/ServiciosRepository.js
+++ src/repositories/ServiciosRepository.js
@@ -1,13 +1,22 @@
 import {getConnection} from '../BD/database.js'
+import sql from "mssql";
+
 
 export default class ServicioRepository{
 
 async getAllServicios() {
-    var query = `SELECT Servicios.Nombre, Descripcion, Foto, Precio, Categorias.Nombre AS CategoriaNombre, Usuarios.Nombre AS UsuarioNombre
-    FROM Servicios INNER JOIN Categorias ON Servicios.idCategoria = Categorias.id INNER JOIN Usuarios ON Servicios.idCreador = Usuarios.id`;
+    var query = `SELECT Servicios.Nombre, Descripcion, Foto, Precio, Categorias.Nombre AS CategoriaNombre, Usuarios.Nombre AS UsuarioNombre,
+    (SELECT [id]
+          ,[Dia]
+          ,[HoraDesde]
+          ,[HoraHasta]
+          ,[idServicio]
+      FROM [dbo].[Disponibilidad]
+      WHERE Disponibilidad.idServicio = Servicios.id FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS Disponibilidad
+      FROM Servicios INNER JOIN Categorias ON Servicios.idCategoria = Categorias.id INNER JOIN Usuarios ON Servicios.idCreador = Usuarios.id`;
     const pool = await getConnection()
-    const values = await pool.request().query(query);
-    return values;
+    const {recordset} = await pool.request().query(query);
+    return recordset;
 }
 async BorrarServicio(id, id_creator_user){
     var query = `DELETE FROM Servicios WHERE id = ${id} AND idCreador = ${id_creator_user}`;
@@ -32,20 +41,19 @@ async EditarServicio(Servicio){
         throw error;
     }
 }
-async CrearServicio(Servicio){
-    var query = `INSERT INTO Servicios (idCreador, idCategoria, Nombre, Descripcion, Foto, Precio) VALUES ($1, $2, $3, $4, $5, $6)`
-    const values = [
-        Servicio.idCreador,
-        Servicio.idCategoria,
-        Servicio.Nombre,
-        Servicio.Descripcion,
-        Servicio.Foto,
-        parseInt(Servicio.Precio)
-    ];
-    console.log(values)
+async CrearServicio(servicio){
+    const query = "INSERT INTO Servicios (idCreador, idCategoria, Nombre, Descripcion, Foto, Precio) VALUES (@idCreador, @idCategoria, @Nombre, @Descripcion, @Foto, @Precio)";
+    const pool = await getConnection()
+    const request = pool.request();
+    request.input('idCreador', sql.Int, servicio.idCreador);
+    request.input('idCategoria', sql.Int, servicio.idCategoria);
+    request.input('Nombre', sql.VarChar, servicio.Nombre);
+    request.input('Descripcion', sql.Text, servicio.Descripcion);
+    request.input('Foto', sql.VarBinary, servicio.Foto);
+    request.input('Precio', sql.Money, servicio.Precio);
+    console.log(request)
     try {
-        const pool = await getConnection()
-        await pool.request().query(query, values);
+        await request.query(query);
         console.log('Servicio agregado');
     } catch (error) {
         console.error('Error al inscribir al servicio', error.stack);
